8522beb7300174d40f4582be3406588922cea042
Mohammad Allam <mallam@cloudinn.net>
2018-07-17 15:49:05 +0200

    dummy2

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -1,12 +1,42 @@
 #!/usr/bin/env bash
 
-# set -x
 
+##################
+## Initializing ##
+##################
 # file names & paths
-tmp="$HOME"  # destination folder to store the final iso file
-hostname="ubuntu"
+working_dir=`pwd -P`
 currentuser="$(whoami)"
+seed_file="preseed.seed"
+#
+# command line args
+input_preseed_file=$1
+username=${2-"cloudinn"}
+password=${3-"cloudinn"}
+hostname=${4-"proxy"}
+timezone=${5-"Etc/UTC"}
+menu_lang=${6-"en"}
+#
+if [ -z $input_preseed_file ] || [ $1 = '-h' ] || [ $1 = '--help' ]; then
+  echo " no preseed file was given"
+  echo "Usage: $0 <preseed_file> [username[password[hostname[timezone[setup menu language]]]]]"
+  echo
+  exit 1
+else
+  input_preseed_file=`realpath $input_preseed_file`
+fi
+#
+# check if the script is running without sudo or root priveleges
+if [ $currentuser != "root" ]; then
+    echo " you need sudo privileges to run this script, or run it as root"
+    exit 1
+fi
 
+
+##########################
+## Function definations ##
+##########################
+#
 # define spinner function for slow tasks
 # courtesy of http://fitnr.com/showing-a-bash-spinner.html
 spinner()
@@ -23,7 +53,7 @@ spinner()
     done
     printf "    \b\b\b\b"
 }
-
+#
 # define download function
 # courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
 download()
@@ -35,7 +65,7 @@ download()
     echo -ne "\b\b\b\b"
     echo " DONE"
 }
-
+#
 # define function to check if program is installed
 # courtesy of https://gist.github.com/JamieMason/4761049
 function program_is_installed {
@@ -47,201 +77,124 @@ function program_is_installed {
     echo $return_
 }
 
-# print a pretty header
-echo
-echo " +---------------------------------------------------+"
-echo " |            UNATTENDED UBUNTU ISO MAKER            |"
-echo " +---------------------------------------------------+"
-echo
 
-# ask if script runs without sudo or root priveleges
-if [ $currentuser != "root" ]; then
-    echo " you need sudo privileges to run this script, or run it as root"
-    exit 1
-fi
-
-#check that we are in ubuntu 16.04
-
-fgrep "16.04" /etc/os-release >/dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-     ub1604="yes"
-fi
-
-#get the latest versions of Ubuntu LTS
-
-tmphtml=$tmp/tmphtml
-rm $tmphtml >/dev/null 2>&1
-wget -O $tmphtml 'http://releases.ubuntu.com/' >/dev/null 2>&1
-
-prec=$(fgrep Precise $tmphtml | head -1 | awk '{print $3}')
-trus=$(fgrep Trusty $tmphtml | head -1 | awk '{print $3}')
-xenn=$(fgrep Xenial $tmphtml | head -1 | awk '{print $3}')
-
-
-download_file="mini.iso"                                       # filename of the iso to be downloaded
-download_location="http://archive.ubuntu.com/ubuntu/dists\
-/xenial/main/installer-amd64/current/images/netboot"  # location of the file to be downloaded
-new_iso_name="mini-unattended.iso"
-
-
-if [ -f /etc/timezone ]; then
-  timezone=`cat /etc/timezone`
-elif [ -h /etc/localtime ]; then
-  timezone=`readlink /etc/localtime | sed "s/\/usr\/share\/zoneinfo\///"`
-else
-  checksum=`md5sum /etc/localtime | cut -d' ' -f1`
-  timezone=`find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$checksum" | sed "s/.*\/usr\/share\/zoneinfo\///" | head -n 1`
-fi
-
-# ask the user questions about his/her preferences
-read -ep " please enter your preferred timezone: " -i "${timezone}" timezone
-read -ep " please enter your preferred username: " -i "netson" username
-read -sp " please enter your preferred password: " password
-printf "\n"
-read -sp " confirm your preferred password: " password2
-printf "\n"
-read -ep " Make ISO bootable via USB: " -i "yes" bootable
-
-# check if the passwords match to prevent headaches
-if [[ "$password" != "$password2" ]]; then
-    echo " your passwords do not match; please restart the script and try again"
-    echo
-    exit
-fi
-
-# download the ubunto iso. If it already exists, do not delete in the end.
-cd $tmp
-if [[ ! -f $tmp/$download_file ]]; then
+#########################
+## Distro ISO download ##
+#########################
+#
+download_file="mini.iso"  # filename of the downloaded iso
+download_location="http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/current/images/netboot/"  # location of the file to be downloaded
+output_iso_name="ubuntu-cloudinn.iso"
+#
+mkdir -p $working_dir
+if [[ ! -f $working_dir/$download_file ]]; then
     echo -n " downloading $download_file: "
     download "$download_location$download_file"
 fi
-if [[ ! -f $tmp/$download_file ]]; then
+if [[ ! -f $working_dir/$download_file ]]; then
 	echo "Error: Failed to download ISO: $download_location$download_file"
 	echo "This file may have moved or may no longer exist."
 	echo
-	echo "You can download it manually and move it to $tmp/$download_file"
+	echo "You can download it manually and move it to $working_dir/$download_file"
 	echo "Then run this script again."
 	exit 1
 fi
 
-# download netson seed file
-seed_file="netson.seed"
-if [[ ! -f $tmp/$seed_file ]]; then
-    echo -n " downloading $seed_file: "
-    download "https://raw.githubusercontent.com/netson/ubuntu-unattended/master/$seed_file"
-fi
 
-# install required packages
-echo " installing required packages"
+###########################
+## Dependencies download ##
+###########################
+#
+installing_apps=''
+#
 if [ $(program_is_installed "mkpasswd") -eq 0 ] || [ $(program_is_installed "mkisofs") -eq 0 ]; then
-    (apt-get -y update > /dev/null 2>&1) &
-    spinner $!
-    (apt-get -y install whois genisoimage > /dev/null 2>&1) &
-    spinner $!
+  installing_apps=$installing_apps' genisoimage'
 fi
-if [[ $bootable == "yes" ]] || [[ $bootable == "y" ]]; then
-    if [ $(program_is_installed "isohybrid") -eq 0 ]; then
-      #16.04
-      if [[ $ub1604 == "yes" || $(lsb_release -cs) == "artful" ]]; then
-        (apt-get -y install syslinux syslinux-utils > /dev/null 2>&1) &
-        spinner $!
-      else
-        (apt-get -y install syslinux > /dev/null 2>&1) &
-        spinner $!
-      fi
-    fi
+#
+if [ $(program_is_installed "mkpasswd") -eq 0 ]; then
+  installing_apps=$installing_apps' mkpasswd'
+fi
+#
+if [ $(program_is_installed "isohybrid") -eq 0 ]; then
+  installing_apps=$installing_apps' syslinux syslinux-utils'
+fi
+#
+if [ $(program_is_installed "7z") -eq 0 ]; then
+  installing_apps=$installing_apps' p7zip-full'
+fi
+#
+if [ ! -z $installing_apps ]; then
+  echo "Installing system dependencies: $installing_apps"
+  (apt-get -y update > /dev/null 2>&1) &
+  spinner $!
+  (apt-get -y install $installing_apps > /dev/null 2>&1) &
+  spinner $!
 fi
 
-
-# create working folders
+######################
+## Working on image ##
+######################
+#
+# create working directories
 echo " remastering your iso file"
-mkdir -p $tmp
-mkdir -p $tmp/iso_org
-mkdir -p $tmp/iso_new
-
-# mount the image
-if grep -qs $tmp/iso_org /proc/mounts ; then
-    echo " image is already mounted, continue"
-else
-    (mount -o loop $tmp/$download_file $tmp/iso_org > /dev/null)
-fi
-
-# copy the iso contents to the working directory
-(cp -rT $tmp/iso_org $tmp/iso_new > /dev/null) &
+tmp_iso_dir=$working_dir/iso_new
+mkdir -p $tmp_iso_dir
+#
+# unpacking image
+(7z x -y -o$tmp_iso_dir $working_dir/$download_file > /dev/null) &
 spinner $!
-
+#
 # set the language for the installation menu
-cd $tmp/iso_new
-#doesn't work for 16.04
-echo en > $tmp/iso_new/isolinux/lang
-
-#16.04
-#taken from https://github.com/fries/prepare-ubuntu-unattended-install-iso/blob/master/make.sh
-sed -i -r 's/timeout\s+[0-9]+/timeout 1/g' $tmp/iso_new/isolinux/isolinux.cfg
-
-
-# set late command
-
-if [[ $ub1604 == "yes" ]]; then
-   late_command="apt-install wget; in-target wget --no-check-certificate -O /home/$username/start.sh https://github.com/netson/ubuntu-unattended/raw/master/start.sh ;\
-     in-target chmod +x /home/$username/start.sh ;"
-else
-   late_command="chroot /target wget -O /home/$username/start.sh https://github.com/netson/ubuntu-unattended/raw/master/start.sh ;\
-     chroot /target chmod +x /home/$username/start.sh ;"
-fi
-
-
-
-# copy the netson seed file to the iso
-cp -rT $tmp/$seed_file $tmp/iso_new/preseed/$seed_file
-
-# include firstrun script
-echo "
-# setup firstrun script
-d-i preseed/late_command                                    string      $late_command" >> $tmp/iso_new/preseed/$seed_file
-
+# doesn't work for 16.04
+echo $menu_lang > $tmp_iso_dir/lang
+#
+# 16.04
+# syslinux auto proceed
+# taken from https://github.com/fries/prepare-ubuntu-unattended-install-iso/blob/master/make.sh
+sed -i -r 's/timeout\s+[0-9]+/timeout 50/g' $tmp_iso_dir/isolinux.cfg
+# sed -i -r 's/prompt\s+[0-9]+/prompt 2/g' $tmp_iso_dir/isolinux.cfg
+#
+# copy the preseed file to the iso
+cp -rT $input_preseed_file $tmp_iso_dir/$seed_file
+#
 # generate the password hash
 pwhash=$(echo $password | mkpasswd -s -m sha-512)
-
-# update the seed file to reflect the users' choices
-# the normal separator for sed is /, but both the password and the timezone may contain it
-# so instead, I am using @
-sed -i "s@{{username}}@$username@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{pwhash}}@$pwhash@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{hostname}}@$hostname@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{timezone}}@$timezone@g" $tmp/iso_new/preseed/$seed_file
-
+#
+# update preseed file
+sed -i "s@{{username}}@$username@g" $tmp_iso_dir/$seed_file
+sed -i "s@{{pwhash}}@$pwhash@g" $tmp_iso_dir/$seed_file
+sed -i "s@{{hostname}}@$hostname@g" $tmp_iso_dir/$seed_file
+sed -i "s@{{timezone}}@$timezone@g" $tmp_iso_dir/$seed_file
+#
 # calculate checksum for seed file
-seed_checksum=$(md5sum $tmp/iso_new/preseed/$seed_file)
-
-# add the autoinstall option to the menu
+seed_checksum=$(md5sum $tmp_iso_dir/$seed_file)
+#
+# remove default menu selection
+sed -i "/menu default/d" $tmp_iso_dir/txt.cfg
+#
+# add our new option to the menu as default
 sed -i "/label install/ilabel autoinstall\n\
-  menu label ^Autoinstall NETSON Ubuntu Server\n\
-  kernel /install/vmlinuz\n\
-  append file=/cdrom/preseed/ubuntu-server.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/netson.seed preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
-
-echo " creating the remastered iso"
-cd $tmp/iso_new
-(mkisofs -D -r -V "NETSON_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp/$new_iso_name . > /dev/null 2>&1) &
+  menu label ^Autoinstall CloudInn Ubuntu\n\
+  menu default\n\
+  kernel linux\n\
+  append initrd=initrd.gz auto=true priority=high preseed/file=$seed_file preseed/file/checksum=$seed_checksum --" "$tmp_iso_dir/txt.cfg"
+#
+# creating the remastered iso
+cd $tmp_iso_dir || exit
+(mkisofs -D -r -V "CLOUDINN_UBUNTU" -cache-inodes -J -l -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $working_dir/$output_iso_name . > /dev/null 2>&1) &
 spinner $!
-
-# make iso bootable (for dd'ing to  USB stick)
-if [[ $bootable == "yes" ]] || [[ $bootable == "y" ]]; then
-    isohybrid $tmp/$new_iso_name
-fi
-
-# cleanup
-umount $tmp/iso_org
-rm -rf $tmp/iso_new
-rm -rf $tmp/iso_org
-rm -rf $tmphtml
-
-
+#
+# make iso bootable (for dd'ing to USB stick)
+isohybrid $working_dir/$output_iso_name
+#
+# cleaning
+# rm -rf $tmp_iso_dir  # XXX uncomment this
+#
 # print info to user
+echo
 echo " -----"
 echo " finished remastering your ubuntu iso file"
-echo " the new file is located at: $tmp/$new_iso_name"
+echo " the new file is located at: $working_dir/$output_iso_name"
 echo " your username is: $username"
 echo " your password is: $password"
 echo " your hostname is: $hostname"
@@ -259,3 +212,4 @@ unset download_location
 unset new_iso_name
 unset tmp
 unset seed_file
+unset working_dir
